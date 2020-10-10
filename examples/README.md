14073535f8183d73aacdb18c7d6fd2fdce6fd05b
joaoppagnan <joaopedroopagnan@gmail.com>
2020-10-10 15:20:08 -0300

    criei um diretorio para armazenarmos as ativiades que fizemos durante o semestre e que serao uteis para o jogo

---
+++ examples/README.md
@@ -0,0 +1,7 @@
+# EA872 - NAV.io
+
+## /examples
+
+Reservei esse diretório para colocarmos exemplos e outras atividades que fizemos/estamos fazendo durante o semestre e que podem auxiliar na hora de montar o nosso jogo.
+
+Os códigos são de simulação física, SDL e mais algumas coisas.
---
+++ examples/massa-mola-amortecedor/massa-mola-amortecedor.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <memory>
+#include <cmath>
+
+#define TEMPO 0.1
+
+using namespace std;
+
+class Massa{
+    private:
+        float mass;
+    public:
+        Massa();
+        void new_mass(float m);
+        float get_mass();
+};
+
+class Mola{
+    private:
+        float tam, elast;
+    public:
+        Mola();
+        void new_spring(float A, float k);
+        float get_tam();
+        float get_elast();
+};
+
+class Amortecedor{
+    private:
+        float dmp;
+    public:
+        Amortecedor();
+        void new_damp(float d);
+        float get_damp();
+};
+
+class Equacoes{
+    private:
+        shared_ptr<Massa> block;
+        shared_ptr<Mola> spring;
+        shared_ptr<Amortecedor> damping;
+    public:
+        Equacoes();
+        void calculo();
+};
+
+void Massa::new_mass(float m){
+    this->mass = m;
+}
+
+float Massa::get_mass(){
+    return mass;
+}
+
+Massa::Massa(){
+}
+
+void Mola::new_spring(float A, float k){
+    this->tam = A;
+    this->elast = k;
+}
+
+float Mola::get_tam(){
+    return tam;
+}
+
+float Mola::get_elast(){
+    return elast;
+}
+
+Mola::Mola(){
+}
+
+void Amortecedor::new_damp(float d){
+    this->dmp = d;
+}
+
+float Amortecedor::get_damp(){
+    return dmp;
+}
+
+Amortecedor::Amortecedor(){   
+}
+
+void Equacoes::calculo(){
+    shared_ptr<Massa> block (new Massa);
+    shared_ptr<Mola> spring (new Mola);
+    shared_ptr<Amortecedor> damper (new Amortecedor);
+
+    block->new_mass(5.0);
+    spring->new_spring(10, 5);
+    damper->new_damp(10);
+
+    float massa, posicao, velocidade, aceleracao, amplitude, elasticidade, amortecimento;
+    float damp_ratio, freq_ang;
+    float forca_mola;
+
+    massa = block->get_mass();
+    elasticidade = spring->get_elast();
+    amplitude = spring->get_tam();
+    amortecimento = damper->get_damp();
+
+    freq_ang = sqrt(elasticidade/massa);
+    damp_ratio = amortecimento / (2*sqrt(massa*elasticidade));
+
+    posicao = amplitude;
+    velocidade = 0;
+    aceleracao = 0;
+
+    forca_mola = - (elasticidade*posicao) - (damp_ratio*velocidade);
+
+    for (int n=0; n <= 200; n++){
+
+        aceleracao = forca_mola/massa;
+        velocidade += aceleracao*TEMPO;
+        posicao += velocidade*TEMPO;
+
+        forca_mola = - (elasticidade*posicao) -(damp_ratio*velocidade);
+
+        cout << "Tempo: " << n*TEMPO << " \t";
+        cout << "Posicao: " << posicao << " \t";
+        cout << "Velocidade: " << velocidade << " \t";
+        cout << "Aceleracao: " << aceleracao << endl;
+    }    
+}
+
+
+Equacoes::Equacoes(){
+}
+
+int main(){
+    shared_ptr<Equacoes> modelo;
+    modelo->calculo();
+    return 0;
+}
